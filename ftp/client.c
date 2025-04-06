43c4b876d3d08430f28316c8950c561ed5690a87
Sandra Simon <sandrasimon2301@gmail.com>
2025-04-06 23:04:51 +0530

    Update client.c

--- ftp/client.c
+++ ftp/client.c
@@ -1,66 +1,67 @@
+// client.c
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <sys/socket.h>
+#include <sys/socket.h>     // For socket(), connect(), send()
 #include <string.h>
-#include <unistd.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <unistd.h>         // For close()
+#include <netinet/in.h>     // For sockaddr_in
+#include <arpa/inet.h>      // For inet_addr()
 
-#define PORT 3621
-#define BUFFER_SIZE 4096
-
-void sendFile(int clientSock) {
-    char buffer[BUFFER_SIZE], filename[128];
-    int bytesRead;
+void sendFile(int client) {
+    char buffer[4098];             // Buffer to hold file content chunks
+    char filename[128];            // To store filename input from user
+    int bytesRead = 0;
     FILE *fp;
 
-    printf("Enter the filename: ");
-    scanf("%s", filename);
+    printf("Enter the Filename: ");
+    scanf("%s", filename);         // Get filename from user
 
-    fp = fopen(filename, "r");
+    fp = fopen(filename, "r");     // Open file in read mode
     if (fp == NULL) {
-        perror("File open error");
+        printf("Error in opening file.\n");
         return;
     }
 
-    printf("Sending file...\n");
-
-    while ((bytesRead = fread(buffer, sizeof(char), BUFFER_SIZE, fp)) > 0) {
-        if (send(clientSock, buffer, bytesRead, 0) < 0) {
-            perror("Send error");
-            break;
-        }
+    // Read the file and send to server in chunks
+    while ((bytesRead = fread(buffer, sizeof(char), sizeof(buffer) - 1, fp)) > 0) {
+        buffer[bytesRead] = '\0';                  // Null terminate the buffer
+        send(client, buffer, strlen(buffer), 0);   // Send the data over socket
     }
 
-    fclose(fp);
-    printf("File sent successfully.\n");
+    printf("File '%s' sent successfully.\n", filename);
+    fclose(fp);        // Close file after sending
 }
 
 int main() {
-    int clientSock;
+    int client;
     struct sockaddr_in servAddr;
+    socklen_t addrSize;
 
-    clientSock = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSock < 0) {
+    // Create a TCP socket
+    client = socket(AF_INET, SOCK_STREAM, 0);
+    if (client < 0) {
         perror("Socket creation failed");
-        exit(1);
+        return -1;
     }
+    printf("Socket created.\n");
 
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_port = htons(PORT);
-    servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    // Server address setup
+    servAddr.sin_family = AF_INET;                 // IPv4
+    servAddr.sin_port = htons(3621);               // Port number (3621)
+    servAddr.sin_addr.s_addr = inet_addr("127.0.0.1");  // Localhost
+    memset(servAddr.sin_zero, 0, sizeof(servAddr.sin_zero));  // Zero padding
+    addrSize = sizeof(servAddr);
 
-    if (connect(clientSock, (struct sockaddr*)&servAddr, sizeof(servAddr)) < 0) {
+    // Connect to the server
+    if (connect(client, (struct sockaddr *)&servAddr, addrSize) < 0) {
         perror("Connection failed");
-        close(clientSock);
-        exit(1);
+        return -1;
     }
-
     printf("Connected to server.\n");
 
-    sendFile(clientSock);
-
-    close(clientSock);
+    sendFile(client);      // Send file content to server
+    close(client);         // Close the socket
     return 0;
 }
